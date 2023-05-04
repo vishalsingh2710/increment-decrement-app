5bf77ef70d33fd1540cfa81f9ab593d03ba88a7f
vish0504 <122441761+vish0504@users.noreply.github.com>
2023-05-04 17:31:01 +0530

    initial message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setUpdatedCount] = useState(0);
+
+  const incrementNumber = () => {
+    setUpdatedCount(count + 1);
+  };
+  const decrementNumber = () => {
+    if (count > 0) {
+      setUpdatedCount(count - 1);
+    } else {
+      alert("Sorry, Zero limit Reached");
+      setUpdatedCount(0);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1> {count}</h1>
+          <div className="btn_div">
+            <button onClick={incrementNumber}>Increment</button>
+            <button onClick={decrementNumber}>Decrement</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
